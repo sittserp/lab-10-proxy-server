094b45f58e44fee45cb9c69c09b7ed959226e277
Perry Sittser <sittserp@gmail.com>
2020-11-12 11:16:47 -0800

    weather and trails working

--- utils.js
+++ utils.js
@@ -7,7 +7,36 @@ function mungeLocation(location) {
     };
 }
 
+function mungeWeather(weatherData) {
+    return weatherData.data.map(item => {
+        return {
+            forecast: item.weather.description,
+            time: item.datetime
+        };
+    }).slice(1, 9);
+}
+
+function mungeTrails(trailData) {
+    return trailData.trails.map(item => {
+        return {
+
+            name: item.name,
+            location: item.location,
+            length: item.length,
+            stars: item.stars,
+            star_votes: item.starVotes,
+            summary: item.summary,
+            trail_url: item.url,
+            conditions: item.conditionStatus,
+            condition_date: item.conditionDate,
+            condition_time: '0:00:00'
+        };
+    }).slice(0, 10);
+}
+
 module.exports = {
-    mungeLocation
+    mungeLocation,
+    mungeWeather,
+    mungeTrails
 };
 
